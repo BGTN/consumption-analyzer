0db7f60090ba0a10e7e796414607a7012c638c7c
Kai Baumgarten <kaibgtn@outlook.de>
2020-12-12 19:46:30 +0100

    add yaml pipeline

---
+++ ConsumptionAnalyzeService/ci/infra/storage-account/deploy-storage.ps1
@@ -0,0 +1,57 @@
+﻿#Requires -Version 3.0
+
+Param(
+    [string] $TemplateFile = 'azuredeploy.json',
+    [string] $TemplateParametersFile = 'azuredeploy.parameters.json',
+    [switch] $ValidateOnly
+)
+
+try {
+    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(' ','_'), '3.0.0')
+} catch { }
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 3
+
+function Format-ValidationOutput {
+    param ($ValidationOutput, [int] $Depth = 0)
+    Set-StrictMode -Off
+    return @($ValidationOutput | Where-Object { $_ -ne $null } | ForEach-Object { @('  ' * $Depth + ': ' + $_.Message) + @(Format-ValidationOutput @($_.Details) ($Depth + 1)) })
+}
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
+$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+
+$OptionalParameters["STORAGE_NAME"] = $env:STORAGE_NAME
+
+# Create the resource group only when it doesn't already exist
+if ((Get-AzResourceGroup -Name $env:STORAGE_RESOURCEGROUP_NAME -Location $env:LOCATION -Verbose -ErrorAction SilentlyContinue) -eq $null) {
+    New-AzResourceGroup -Name $env:STORAGE_RESOURCEGROUP_NAME -Location $env:LOCATION -Verbose -Force -ErrorAction Stop
+}
+
+if ($ValidateOnly) {
+    New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                       -ResourceGroupName $env:STORAGE_RESOURCEGROUP_NAME `
+                                       -TemplateFile $TemplateFile `
+                                       -TemplateParameterFile $TemplateParametersFile `
+                                       @OptionalParameters `
+                                       -Force -Verbose `
+                                       -ErrorVariable ErrorMessages `
+                                       -WhatIf
+    if ($ErrorMessages) {
+        Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
+    }
+}
+else {
+    New-AzResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                       -ResourceGroupName $env:STORAGE_RESOURCEGROUP_NAME `
+                                       -TemplateFile $TemplateFile `
+                                       -TemplateParameterFile $TemplateParametersFile `
+                                       @OptionalParameters `
+                                       -Force -Verbose `
+                                       -ErrorVariable ErrorMessages
+    if ($ErrorMessages) {
+        Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
+    }
+}
---
+++ core-ci/variables/setvariables.ps1
@@ -0,0 +1,20 @@
+param (
+	[String]$env
+)
+
+function WriteVarToHost($key, $value) {
+	Write-Host $key = $value
+	Write-Host "##vso[task.setvariable variable=$key]$value"
+	[Environment]::SetEnvironmentVariable($key, $value)
+}
+
+
+$location = 'northeurope'
+WriteVarToHost 'LOCATION' $location
+$locationShort = 'neu'
+
+$STORAGE_RESOURCEGROUP_NAME = "consumption_analyzer_storage_" + $env + "_" + $location + "_rg"
+WriteVarToHost 'STORAGE_RESOURCEGROUP_NAME' $STORAGE_RESOURCEGROUP_NAME
+
+$STORAGE_NAME = "consmptanalyzerst$env$locationShort"
+WriteVarToHost 'STORAGE_NAME' $STORAGE_NAME
